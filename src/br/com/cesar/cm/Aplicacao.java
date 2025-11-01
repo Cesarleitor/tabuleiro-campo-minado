fa59150f8562e2cce169ac032b73404108b86d4f
Cesar Augusto <100009078+Cesarleitor@users.noreply.github.com>
2025-11-01 20:23:46 -0300

    criado campo

---
+++ src/br/com/cesar/cm/Aplicacao.java
@@ -0,0 +1,17 @@
+package br.com.cesar.cm;
+
+import br.com.cesar.cm.modelo.Tabuleiro;
+import br.com.cesar.cm.visao.TabuleiroConsole;
+
+public class Aplicacao {
+	
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		new TabuleiroConsole(tabuleiro);
+		
+		
+	}
+
+}
+
